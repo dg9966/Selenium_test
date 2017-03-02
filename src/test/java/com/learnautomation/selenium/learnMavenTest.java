680d2dee480638c904234ebe6eb3771c0c66357f
Debasish <dg9966@gmail.com>
2017-03-02 15:20:23 +0530

    Testcase updated

---
+++ src/test/java/com/learnautomation/selenium/learnMavenTest.java
@@ -0,0 +1,14 @@
+package com.learnautomation.selenium;
+
+import org.testng.annotations.Test;
+
+public class learnMavenTest {
+	
+	
+	@Test
+	public void mavenTest()
+	{
+		System.out.println("Welcome to Meven World");
+	}
+
+}
